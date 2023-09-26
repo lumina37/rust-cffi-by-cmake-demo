e2c5ac191540a1c164b43f60eb85f3f767f96d16
Starry-OvO <starry.qvq@gmail.com>
2023-09-26 14:01:59 +0800

    refactor: simplify CMakeLists.txt

--- src/libfoo/bar.c
+++ src/libfoo/bar.c
@@ -2,4 +2,6 @@
 
 #include "libfoo/bar.h"
 
-void testcall(float value) { printf("Hello world from C! Value passed: %f\n", value); }
+void testcall(float value) {
+    printf("Hello world from C! Value passed: %f\n", value);
+}
